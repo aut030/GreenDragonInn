0cd70f21e67c620deb6c22e1c8e04ca69931b60f
aut030 <dvd.carey@att.net>
2023-08-28 01:39:34 -0500

    image update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import './Button.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src='videos/nz-hills.mp4' autoPlay loop muted />
+        <video src='https://aut030.github.io/GreenDragonInn/videos/nz-hills.mp4' autoPlay loop muted />
         <h1>ADVENTURE AWAITS!</h1>
         <p>What are you waiting for?</p>
         <div className='hero-btns'>
