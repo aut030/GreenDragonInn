04f4ff9e69613f9dda4533c01a0d27ffb1147931
aut030 <dvd.carey@att.net>
2023-08-28 13:59:02 -0500

    image test 2.2

--- src/components/SideScroll.js
+++ src/components/SideScroll.js
@@ -15,43 +15,43 @@ function SideScroll() {
       <ul className='media-scroller'>
         <li>
           <button className='media-element' onClick={openQuestWindow}>
-            <img src='https://raw.githubusercontent.com/aut030/GreenDragonInn/main/public/images/dragon.jpg' alt='dragon'/>
+            <img src='docs/assets/dragon.jpg' alt='dragon'/>
             <p className='title'>Quest</p>
           </button>
         </li>
         <li>
           <button className='media-element' onClick={openQuestWindow}>
-            <img src='https://github.com/aut030/GreenDragonInn/blob/main/public/images/dragon.jpg' alt='dragon'/>
+            <img src='/docs/assets/dragon.jpg' alt='dragon'/>
             <p className='title'>Quest</p>
           </button>
         </li>
         <li>
           <button className='media-element' onClick={openQuestWindow}>
-            <img src='https://github.com/aut030/GreenDragonInn/tree/main/public/images/dragon.jpg' alt='dragon'/>
+            <img src='./docs/assets/dragon.jpg' alt='dragon'/>
             <p className='title'>Quest</p>
           </button>
         </li>
         <li>
           <button className='media-element' onClick={openQuestWindow}>
-            <img src='https://aut030.github.io/GreenDragonInn/images/dragon.jpg' alt='dragon'/>
+            <img src='../../docs/assets/dragon.jpg' alt='dragon'/>
             <p className='title'>Quest</p>
           </button>
         </li>
         <li>
           <button className='media-element' onClick={openQuestWindow}>
-            <img src='https://aut030.github.io/images/dragon.jpg' alt='dragon'/>
+            <img src='assets/dragon.jpg' alt='dragon'/>
             <p className='title'>Quest</p>
           </button>
         </li>
         <li>
           <button className='media-element' onClick={openQuestWindow}>
-            <img src='https://aut030.github.io/GreenDragonInn/public/images/dragon.jpg' alt='dragon'/>
+            <img src='./assets/dragon.jpg' alt='dragon'/>
             <p className='title'>Quest</p>
           </button>
         </li>
         <li>
           <button className='media-element' onClick={openQuestWindow}>
-            <img src='../../public/images/dragon.jpg' alt='dragon'/>
+            <img src='/dragon.jpg' alt='dragon'/>
             <p className='title'>Quest</p>
           </button>
         </li>
